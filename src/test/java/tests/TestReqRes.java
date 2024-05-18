4f707e4d7ba7edaf53b23485c8316673d15827a5
LRaileanu <lucrai@yahoo.com>
2024-05-18 11:40:37 +0300

    test

---
+++ src/test/java/tests/TestReqRes.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TestReqRes {
+
+
+    @Test
+    public void testTestNG() {
+        System.out.println("Hello");
+
+    }
+
+
+}
